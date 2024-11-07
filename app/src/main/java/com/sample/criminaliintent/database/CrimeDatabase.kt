c81d92f852ea12d64879e4b58665da809ef54dd5
Nextkick <Ba1aBoJI@mail.ru>
2024-11-07 19:37:51 +0700

    Инициализация класса CrimeDatabase + Добавление функций TypeConverter и включение TypeConverter (Определение баз данных и таблиц)

---
+++ app/src/main/java/com/sample/criminaliintent/database/CrimeDatabase.kt
@@ -0,0 +1,11 @@
+package com.sample.criminaliintent.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.sample.criminaliintent.Crime
+
+@Database(entities = [ Crime::class ], version=1)
+@TypeConverters(CrimeTypeConverters::class)
+abstract class CrimeDatabase : RoomDatabase() {
+}
