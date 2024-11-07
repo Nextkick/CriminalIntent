244ad16b6ce2f27a754050176e5bf76cde3dc759
Nextkick <Ba1aBoJI@mail.ru>
2024-11-07 19:27:47 +0700

    Добавление зависимостей + Превращаем Crime в сущность

--- app/src/main/java/com/sample/criminaliintent/Crime.kt
+++ app/src/main/java/com/sample/criminaliintent/Crime.kt
@@ -1,7 +1,13 @@
 package com.sample.criminaliintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 import java.util.UUID
 
-class Crime(val id: UUID = UUID.randomUUID(), var title: String = "", var date: Date = Date(), var isSolved: Boolean = false) {
+@Entity
+data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false) {
 }
