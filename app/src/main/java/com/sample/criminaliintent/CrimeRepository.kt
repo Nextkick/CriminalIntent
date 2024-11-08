603ea2a49b2e6e350b7b48df9249716442aed9d9
Nextkick <Ba1aBoJI@mail.ru>
2024-11-09 00:57:52 +0700

    Создание подкласса приложения+Подключение его в манифест и настройка свойств репозитория

--- app/src/main/java/com/sample/criminaliintent/CrimeRepository.kt
+++ app/src/main/java/com/sample/criminaliintent/CrimeRepository.kt
@@ -1,8 +1,18 @@
 package com.sample.criminaliintent
 
 import android.content.Context
+import androidx.room.Room
+import com.sample.criminaliintent.database.CrimeDatabase
 
+private const val DATABASE_NAME = "crimedatabase"
 class CrimeRepository private  constructor(context: Context){
+    private val database : CrimeDatabase =
+        Room.databaseBuilder(
+            context.applicationContext,
+            CrimeDatabase::class.java,
+            DATABASE_NAME
+        ).build()
+    private val crimeDao = database.crimeDao()
     companion object {
         private var INSTANCE: CrimeRepository? = null
         fun initialize(context: Context) {
