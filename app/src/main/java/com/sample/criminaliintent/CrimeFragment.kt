d863eceafe666d1b2a68ea6ec79ad3b40ca2444f
Nextkick <Ba1aBoJI@mail.ru>
2024-11-09 13:51:41 +0700

    Получение идентификатора преступления из аргументов и Добавление ViewModel в CrimeFragment

--- app/src/main/java/com/sample/criminaliintent/CrimeFragment.kt
+++ app/src/main/java/com/sample/criminaliintent/CrimeFragment.kt
@@ -3,6 +3,7 @@ package com.sample.criminaliintent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment
 import java.util.UUID
 
 private const val ARG_CRIME_ID = "crime_id"
+private const val TAG = "CrimeFragment"
 
 class CrimeFragment: Fragment() {
     private lateinit var crime: Crime
@@ -22,6 +24,9 @@ class CrimeFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
+        val crimeId: UUID = arguments?.getSerializable(ARG_CRIME_ID) as UUID
+        Log.d(TAG, "args bundle crime ID:$crimeId")
+// Загрузка преступления из базы данных
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_crime, container, false)
