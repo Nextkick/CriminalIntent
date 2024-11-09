576abc993a35f6927bf9d1382a2bf4a5cd19afec
Nextkick <Ba1aBoJI@mail.ru>
2024-11-09 12:27:42 +0700

    Добавление интерфейса обратного вызова + Вызов всех обратных вызовов + Реализация обратных вызовов в Main

--- app/src/main/java/com/sample/criminaliintent/CrimeListFragment.kt
+++ app/src/main/java/com/sample/criminaliintent/CrimeListFragment.kt
@@ -1,5 +1,6 @@
 package com.sample.criminaliintent
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,14 +14,23 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.UUID
 
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment: Fragment() {
+    interface Callbacks {
+        fun onCrimeSelected(crimeId: UUID)
+    }
+    private var callbacks: Callbacks? = null
     private lateinit var crimeRecyclerView: RecyclerView
     private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
     private val crimeListViewModel:
             CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
+    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
@@ -41,6 +51,10 @@ class CrimeListFragment: Fragment() {
                     updateUI(crimes)
                 }
             })
+    }
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
     }
 
     private fun updateUI(crimes: List<Crime>) {
@@ -72,9 +86,7 @@ class CrimeListFragment: Fragment() {
         }
 
         override fun onClick(v: View?) {
-            Toast.makeText(context,
-                "${crime.title} pressed!",
-                Toast.LENGTH_SHORT).show()
+            callbacks?.onCrimeSelected(crime.id)
         }
     }
     private inner class CrimeAdapter(var crimes: List<Crime>): RecyclerView.Adapter<CrimeHolder>(){
--- app/src/main/java/com/sample/criminaliintent/MainActivity.kt
+++ app/src/main/java/com/sample/criminaliintent/MainActivity.kt
@@ -2,8 +2,11 @@ package com.sample.criminaliintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.util.UUID
 
-class MainActivity : AppCompatActivity() {
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,5 +15,9 @@ class MainActivity : AppCompatActivity() {
             val fragment = CrimeListFragment.newInstance()
             supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
         }
+    }
+    override fun onCrimeSelected(crimeId: UUID)
+    {
+        Log.d(TAG, "MainActivity.onCrimeSelected: $crimeId")
     }
 }
